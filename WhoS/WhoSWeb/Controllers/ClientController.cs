e8ea21e97bd137e00a1cdc25cbaa6fd1345fbe31
JeongInHye <inhyea36@naver.com>
2019-03-14 09:59:11 +0900

    webapi -> web으로 이동

---
+++ WhoS/WhoSWeb/Controllers/ClientController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    public class ClientController : Controller
+    {
+        private Hashtable hashtable;
+        private DataBase dataBase;
+
+        [Route("Client/Select")]
+        [HttpGet]
+        public ActionResult<ArrayList> Select()
+        {
+            dataBase = new DataBase();
+            SqlDataReader sdr = dataBase.Reader("sp_ClientSelect");
+
+            ArrayList list = new ArrayList();
+            while (sdr.Read())
+            {
+                string[] arr = new string[sdr.FieldCount];
+                for (int i = 0; i < sdr.FieldCount; i++)
+                {
+                    arr[i] = sdr.GetValue(i).ToString();
+                }
+                list.Add(arr);
+            }
+            dataBase.ReaderClose(sdr);
+            dataBase.Close();
+            return list;
+        }
+
+        [Route("Client/Add")]
+        [HttpPost]
+        public ActionResult<string> Add([FromForm]string Name, [FromForm]string pName, [FromForm]string Call, [FromForm]string Address, [FromForm]string ID, [FromForm]string Password)
+        {
+            hashtable = new Hashtable();
+            hashtable.Add("@cName", Name);
+            hashtable.Add("@cPName", pName);
+            hashtable.Add("@cCall", Call);
+            hashtable.Add("@cAddress", Address);
+            hashtable.Add("@cID", ID);
+            hashtable.Add("@cPassword", Password);
+
+
+            dataBase = new DataBase();
+            if (dataBase.NonQuery("sp_ClientAdd", hashtable) == 1)
+            {
+                dataBase.Close();
+                return "1";
+            }
+            else
+            {
+                dataBase.Close();
+                return "0";
+            }
+        }
+
+
+        [Route("Client/Delete")]
+        [HttpPost]
+        public ActionResult<string> Delete([FromForm]int cNo)
+        {
+            hashtable = new Hashtable();
+            hashtable.Add("@cNo", cNo);
+
+            dataBase = new DataBase();
+            if (dataBase.NonQuery("sp_ClientDelete", hashtable) == 1)
+            {
+                dataBase.Close();
+                return "1";
+            }
+            else
+            {
+                dataBase.Close();
+                return "0";
+            }
+        }
+
+        [Route("Client/ClientEdeitSelect")]
+        [HttpPost]
+        public ActionResult<ArrayList> ClientEdeitSelect([FromForm] int cNo)
+        {
+            dataBase = new DataBase();
+            hashtable = new Hashtable();
+            hashtable.Add("@cNo", cNo);
+            SqlDataReader sdr = dataBase.Reader("sp_ClientEditSelect", hashtable);
+            ArrayList list = new ArrayList();
+
+            while (sdr.Read())
+            {
+                string[] arr = new string[sdr.FieldCount];
+                for (int i = 0; i < sdr.FieldCount; i++)
+                {
+                    arr[i] = sdr.GetValue(i).ToString();
+                }
+                list.Add(arr);
+            }
+            dataBase.ReaderClose(sdr);
+            dataBase.Close();
+            return list;
+        }
+
+        [Route("Client/Edit")]
+        [HttpPost]
+        public ActionResult<string> Edit([FromForm]int cNo, [FromForm]string cName, [FromForm]string cPName, [FromForm]string cCall, [FromForm]string cAddress, [FromForm]string cID, [FromForm]string cPassword)
+        {
+            hashtable = new Hashtable();
+            hashtable.Add("@cNo", cNo);
+            hashtable.Add("@cName", cName);
+            hashtable.Add("@cPName", cPName);
+            hashtable.Add("@cCall", cCall);
+            hashtable.Add("@cAddress", cAddress);
+            hashtable.Add("@cID", cID);
+            hashtable.Add("@cPassword", cPassword);
+
+            dataBase = new DataBase();
+            if (dataBase.NonQuery("sp_ClientEdit", hashtable) == 1)
+            {
+                dataBase.Close();
+                return "1";
+            }
+            else
+            {
+                dataBase.Close();
+                return "0";
+            }
+        }
+
+    }
+}
---
+++ WhoS/WhoSWeb/Controllers/LocationController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private DataBase dataBase;
+        private Hashtable hashtable;
+
+        [Route("Location/AllSelect")]
+        [HttpGet]
+        public ActionResult<ArrayList> LocationAllSelect()
+        {
+            dataBase = new DataBase();
+            SqlDataReader sdr = dataBase.Reader("sp_LocationAllSelect");
+
+            ArrayList list = new ArrayList();
+            while (sdr.Read())
+            {
+                string[] arr = new string[sdr.FieldCount];
+                for (int i = 0; i < sdr.FieldCount; i++)
+                {
+                    arr[i] = sdr.GetValue(i).ToString();
+                    if (i == 3)
+                    {
+                        string str = sdr.GetValue(3).ToString();
+                        string substring = str.Substring(0, 11);
+                        arr[i] = substring;
+                    }
+                    if (i == 4)
+                    {
+                        string str = sdr.GetValue(4).ToString();
+                        string substring = str.Substring(0, 11);
+                        arr[i] = substring;
+                    }
+                }
+                list.Add(arr);
+            }
+            dataBase.ReaderClose(sdr);
+            dataBase.Close();
+            return list;
+        }
+
+        [Route("Location/LocationBtn")]
+        [HttpPost]
+        public ActionResult<ArrayList> ClientEdeitSelect([FromForm] string lNo)
+        {
+            dataBase = new DataBase();
+            hashtable = new Hashtable();
+            hashtable.Add("@lNo", lNo);
+            SqlDataReader sdr = dataBase.Reader("sp_LocationBtn", hashtable);
+            ArrayList list = new ArrayList();
+
+            while (sdr.Read())
+            {
+                string[] arr = new string[sdr.FieldCount];
+                for (int i = 0; i < sdr.FieldCount; i++)
+                {
+                    arr[i] = sdr.GetValue(i).ToString();
+                    if (i == 3)
+                    {
+                        string str = sdr.GetValue(3).ToString();
+                        string substring = str.Substring(0, 11);
+                        arr[i] = substring;
+                    }
+                    if (i == 4)
+                    {
+                        string str = sdr.GetValue(4).ToString();
+                        string substring = str.Substring(0, 11);
+                        arr[i] = substring;
+                    }
+                }
+                list.Add(arr);
+            }
+            dataBase.ReaderClose(sdr);
+            dataBase.Close();
+            return list;
+        }
+
+        [Route("test/test")]
+        [HttpGet]
+        public ActionResult<ArrayList> test()
+        {
+            dataBase = new DataBase();
+            SqlDataReader sdr = dataBase.Reader("test");
+
+            ArrayList list = new ArrayList();
+            while (sdr.Read())
+            {
+                string[] arr = new string[sdr.FieldCount];
+                for (int i = 0; i < sdr.FieldCount; i++)
+                {
+                    arr[i] = sdr.GetValue(i).ToString();
+                    
+                }
+                list.Add(arr);
+            }
+            dataBase.ReaderClose(sdr);
+            dataBase.Close();
+            return list;
+        }
+    }
+}
